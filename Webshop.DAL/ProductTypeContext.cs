e49051cbff9a045b414b8483e9d67593b259d0fb
MartinRuis <ruismartin98@gmail.com>
2018-11-30 14:21:05 +0100

    Dal updates

---
+++ Webshop.DAL/ProductTypeContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webshop.Models;
+using MySql.Data.MySqlClient;
+
+namespace Webshop.DAL
+{
+    public class ProductTypeContext : SqlContext
+    {
+        public bool Create(string product_name)
+        {
+            List<MySqlParameter> parameters = new List<MySqlParameter>();
+            parameters.Add(new MySqlParameter("product_name", MySqlDbType.VarChar) { Value = product_name });
+
+            try
+            {
+                ExecuteInputProc("create_product_type", parameters);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public ProductType Get(Product product)
+        {
+            List<MySqlParameter> parameters = new List<MySqlParameter>();
+            parameters.Add(new MySqlParameter("producttype_id1", MySqlDbType.Int32) { Value = product.ProductTypeID });
+            List<object> o = ExecuteOutputProc("get_product_type", parameters, 1, new string[] { "product_type_name"});
+            return new ProductType { TypeID = product.ProductTypeID, TypeName = o[0].ToString() };
+        }
+
+        public List<ProductType> GetAllTypes()
+        {
+            List<MySqlParameter> parameters = null;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Webshop.Models/ProductType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop.Models
+{
+    public class ProductType
+    {
+        private int typeID;
+        private string typeName;
+
+        public int TypeID { get => typeID; set => typeID = value; }
+        public string TypeName { get => typeName; set => typeName = value; }
+    }
+}
